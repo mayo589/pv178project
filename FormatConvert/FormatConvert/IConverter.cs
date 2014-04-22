2aa84a8f70fda2aac1dc03d575d2d45858e1c252
mayo589 <marekmihalech@gmail.com>
2014-04-22 13:31:13 +0200

    update interface. added resize ratio

--- FormatConvert/FormatConvert/IConverter.cs
+++ FormatConvert/FormatConvert/IConverter.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+//Marek Mihalech
 
 namespace FormatConvert
 {
     interface IConverter
     {
-        void ConvertImagesToFormat(string directory, FileTypes type);
-        void ConvertImagesToFormat(List<string> listOfFiles, FileTypes type);
+        void StartApp(string directory, bool overWrite);
+        void StartApp(List<string> listOfFiles, bool overWrite);
+
+        BitmapSource LoadImage(string fileNameAndPath);
+        BitmapSource SaveImage(string fileNameAndPath, bool overWrite);
+
+        BitmapSource ConvertImagesToFormat(BitmapSource image, FileTypes outputFormat);
+        BitmapSource ResizeImage(BitmapSource image, int newWidth, int newHeight, bool overZoom);
         
+        void WriteToLog(string fileName, string info);
+
+
 
     }
 }
--- FormatConvert/FormatConvert/Program.cs
+++ FormatConvert/FormatConvert/Program.cs
@@ -17,64 +17,42 @@ namespace FormatConvert
         static void Main(string[] args)
         {
             // Open a Stream and decode a JPEG image
-            Stream imageStreamSource = new FileStream("source/Penguins.jpg", FileMode.Open, FileAccess.Read, FileShare.Read);
+            Stream imageStreamSource = new FileStream("source/chnagedims.jpg", FileMode.Open, FileAccess.Read, FileShare.Read);
             JpegBitmapDecoder decoder = new JpegBitmapDecoder(imageStreamSource, BitmapCreateOptions.PreservePixelFormat, BitmapCacheOption.Default);
             BitmapSource bitmapSource = decoder.Frames[0];
 
-            // Draw the Image
-            /*Image myImage = new Image();
-            myImage.Source = bitmapSource;
-            myImage.Stretch = Stretch.None;
-            myImage.Margin = new Thickness(20);
-
-            */
-
+        
+            Console.WriteLine(bitmapSource.Width); Console.WriteLine(bitmapSource.Height);
             
+            int width = 1500;
+            int height = 2000;
+
+            double ratioW = width / bitmapSource.Width;
+            double ratioH = height / bitmapSource.Height;
+
+            double ratio = ratioW < ratioH ? ratioW : ratioH;
+            Console.WriteLine(ratio);
+            Console.WriteLine(bitmapSource.DpiX);
+            Console.WriteLine(bitmapSource.DpiY);
 
 
-           /* int width = 128;
-            int height = 128;
-            int stride = width;
-            byte[] pixels = new byte[height * stride];
-            */
-            // Define the image palette
-           // BitmapPalette myPalette = BitmapPalettes.Halftone256;
-
-            // Creates a new empty image with the pre-defined palette
-
-            /*BitmapSource image = BitmapSource.Create(
-                width,
-                height,
-                96,
-                96,
-                PixelFormats.Indexed8,
-                myPalette,
-                pixels,
-                stride);*/
-
-            FileStream stream = new FileStream("output/newww.png", FileMode.Create);
+            FileStream stream = new FileStream("output/chnagedims.png", FileMode.Create);
             PngBitmapEncoder encoder = new PngBitmapEncoder();
-            //TextBlock myTextBlock = new TextBlock();
-            //myTextBlock.Text = "Codec Author is: " + encoder.CodecInfo.Author.ToString();
-            //JpegBitmapEncoder en = new JpegBitmapEncoder(); en.Quality....... na kompresiu
+
             var target = new TransformedBitmap(
                             bitmapSource,
                             new ScaleTransform(
-                               100 / bitmapSource.Width * 96 / bitmapSource.DpiX,
-                                100/ bitmapSource.Height * 96 / bitmapSource.DpiY,
+                             (ratio * bitmapSource.Width) / bitmapSource.Width * bitmapSource.DpiX / bitmapSource.DpiX,
+                             (ratio * bitmapSource.Height) / bitmapSource.Height * bitmapSource.DpiY / bitmapSource.DpiY,
                                 0, 0
 
-                         ));
-            //bitmapSource.Height = 100;
-
+                         ));            
             encoder.Interlace = PngInterlaceOption.On;
             encoder.Frames.Add(BitmapFrame.Create(target));
             encoder.Save(stream);
 
-
-
-
             
+            Console.ReadKey();
 
         }
     }
